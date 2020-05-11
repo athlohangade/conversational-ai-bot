299c8ec4b95a0b5e772906110b61ad7b8df83f7a
Adnu100 <dhamangaonadnesh@gmail.com>
2020-05-11 23:17:26 +0530

    fixed bug: only first line displaying page now displays all results given by rasa

--- searchbot-ui/src/app/app.module.ts
+++ searchbot-ui/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { WelcomeComponent } from './welcome/welcome.component';
   imports: [
     BrowserModule,
 	AppRoutingModule,
-	HttpClientModule
+	HttpClientModule,
+	FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
