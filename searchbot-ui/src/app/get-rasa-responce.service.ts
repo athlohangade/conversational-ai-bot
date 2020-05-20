251a909956158dffa33efe759b71e70dc0b7f3f4
Adnu100 <dhamangaonadnesh@gmail.com>
2020-05-20 13:49:41 +0530

    unique user id created for each user, using time till miliseconds

--- searchbot-ui/src/app/get-rasa-responce.service.ts
+++ searchbot-ui/src/app/get-rasa-responce.service.ts
@@ -8,12 +8,19 @@ import { HttpClient } from '@angular/common/http';
 export class GetRasaResponceService {
 
 	RASA_URL: string = "/webhooks/rest/webhook";
+	USERNAME: string;
 
-	constructor(private http: HttpClient) { }
+	constructor(private http: HttpClient) {
+		var d: Date = new Date();
+		var randomNum: number = Math.round(Math.random() * 1000);
+		var name: string = `user#${randomNum}:${d.getUTCHours()}:${d.getUTCMinutes()}:${d.getUTCSeconds()}:${d.getUTCMilliseconds()}`;
+		this.USERNAME = name;
+		console.log(`username: ${this.USERNAME}`);
+	}
 
 	sendMessage(message: string): Observable<any> {
 		var data = {
-			"sender": "Me",
+			"sender": this.USERNAME,
 			"message": message
 		};
 		return this.http.post(this.RASA_URL, data)
