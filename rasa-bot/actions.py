81def42bde803b96449ad10faa6ab7d51137a015
Ishwari Dhondge <icdhondge28@gmail.com>
2020-05-22 15:04:03 +0530

    fallback to faq

--- rasa-bot/actions.py
+++ rasa-bot/actions.py
@@ -5,8 +5,6 @@
 # https://rasa.com/docs/rasa/core/actions/#custom-actions/
 
 
-# This is a simple example for a custom action which utters "Hello World!"
-
 from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
@@ -26,19 +24,6 @@ import json
 from datetime import datetime, timedelta
 import _thread
 import time
-
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
 
 class ActionGetSupport(Action):
     
@@ -79,8 +64,8 @@ class ActionGetSupport(Action):
             answers = OtherSupport.searchInFAQ(msg)
             answers = answers[0]
             for answer in answers :
-                dispatcher.utter_message(text = answer) 
-            return []
+                dispatcher.utter_message(text = answer)
+            return [FollowupAction('action_listen')]
 
         to_reset = False
 
